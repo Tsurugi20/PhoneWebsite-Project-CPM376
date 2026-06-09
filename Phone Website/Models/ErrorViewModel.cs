0a930ac61b42ce0e68d6eb973741814c52bdae7e
Blade <lamt3236@gmail.com>
2026-06-09 11:48:25 +0700

    Phone Website Submit

---
+++ Phone Website/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Phone_Website.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Phone Website/Models/Status.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Phone_Website.Models;
+
+public partial class Status
+{
+    public int Id { get; set; }
+}
