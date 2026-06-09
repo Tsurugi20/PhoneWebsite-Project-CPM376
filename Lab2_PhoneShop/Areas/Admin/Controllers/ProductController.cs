f864b28451f8a6cfc2fe195a65b8f45479e71325
Blade <lamt3236@gmail.com>
2026-06-09 12:05:52 +0700

    Exercise_3

--- Lab2_PhoneShop/Areas/Admin/Controllers/ProductController.cs
+++ Lab2_PhoneShop/Areas/Admin/Controllers/ProductController.cs
@@ -1,8 +1,9 @@
-﻿using Lab2_PhoneShop.Models;
+using Lab2_PhoneShop.Models;
 using Lab2_PhoneShop.PhoneShopDB;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Lab2_PhoneShop.DTOs.Products;
 
 namespace Lab2_PhoneShop.Areas.Admin.Controllers
 {
@@ -10,10 +11,12 @@ namespace Lab2_PhoneShop.Areas.Admin.Controllers
     public class ProductController : Controller
     {
         PhoneShopDBContext db;
+        IWebHostEnvironment env;
 
-        public ProductController(PhoneShopDBContext db)
+        public ProductController(PhoneShopDBContext db, IWebHostEnvironment env)
         {
             this.db = db;
+            this.env = env;
         }
 
         public async Task<ActionResult> Index()
@@ -30,12 +33,55 @@ namespace Lab2_PhoneShop.Areas.Admin.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Create(Product product)
+        public async Task<IActionResult> Create(CreateProductDTO p)
         {
             if (!ModelState.IsValid)
             {
-                return View(product);
+                return View(p);
             }
+
+            string? imgFileName = string.Empty;
+            if (p.Photo != null && p.Photo.Length > 0)
+            {
+                try
+                {
+                    var imgFolder = Path.Combine(env.WebRootPath, "img");
+                    if (!Directory.Exists(imgFolder))
+                    {
+                        Directory.CreateDirectory(imgFolder);
+                    }
+
+                    string? imgPath = Path.Combine(imgFolder, p.Photo.FileName);
+
+                    using (var fs = new FileStream(imgPath, FileMode.Create))
+                    {
+                        await p.Photo.CopyToAsync(fs);
+                    }
+                    imgFileName = p.Photo.FileName;
+                }
+
+                catch (Exception ex)
+                {
+                    ViewBag.Message = $"Error uploading image:" + ex.Message;
+                    return View(p);
+                }
+            }
+
+            Category? cate = null;
+            if (p.CategoryId.HasValue)
+            {
+                cate = await db.Categories.FindAsync(p.CategoryId.Value);
+            }
+
+            var product = new Product
+            {
+                Name = p.Name,
+                Description = p.Description,
+                Price = p.Price,
+                //sal = p.PriceSale,
+                Category = cate,
+                Photo = imgFileName
+            };
 
             await db.Products.AddAsync(product);
             await db.SaveChangesAsync();
---
+++ Lab2_PhoneShop/DTOs/Products/CreateProductDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace Lab2_PhoneShop.DTOs.Products
+{
+    public class CreateProductDTO
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public decimal? Price { get; set; }
+        public decimal? PriceSale { get; set; }
+        public IFormFile? Photo { get; set; }
+        public int? CategoryId { get; set; }
+    }
+}
